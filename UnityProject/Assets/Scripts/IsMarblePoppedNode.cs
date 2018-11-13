919079031bb6c058614949a249c2a606c15db1e8
Matthew Wagar <mrw77@drexel.edu>
2018-11-13 03:14:54 -0500

    Created the Storygraph and basic interactions

---
+++ UnityProject/Assets/Scripts/IsMarblePoppedNode.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StoryGraph;
+
+public class IsMarblePoppedNode : StoryNode
+{
+    public SphereAppear Marble;
+
+
+    #if UNITY_EDITOR    
+    public override string MenuName {get{return "Condition/Is Marble Popped";}}
+
+    public override void SetStyles()
+    {
+        base.SetStyles();    
+        nodeHeaderStyle = StoryGraphStyles.NodeConditionStyle();
+    }
+    public override void SetSerializedProperties()
+    {    
+        AddSerializedProperty("Marble", StorySerializedPropertyType.NoLabelPropertyField);
+    }
+    #endif
+
+    public override void Execute()
+    {
+        if(!Marble.Popped)
+        {
+            GoToNextNode();
+        }
+    }
+
+}
