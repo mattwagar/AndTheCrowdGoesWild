6061b93c139ca718fc83ba1946472a118750ab4e
DESKTOP-EK807HG\tinys <tinysquiddle@gmail.com>
2018-11-20 17:27:48 -0500

    Added Y transform lock and sprite shadows

---
+++ UnityProject/Assets/PreventYMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreventYMovement : MonoBehaviour {
+
+	// Use this for initialization
+	Transform cloudTransform;
+	public float yPosLock = 41.77f;
+	void Start () {
+		cloudTransform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cloudTransform.position = new Vector3(cloudTransform.position.x, yPosLock, cloudTransform.position.z);
+	}
+}
