025b0a6f9dcfbafe49b9529524294fe697ab3ea6
Matthew Napolillo <mnapolillo42@gmail.com>
2018-11-27 21:04:26 -0500

    Implemented sparking and flash on flame marble crush

--- UnityProject/Assets/Scripts/PopFireMarbleEvent.cs
+++ UnityProject/Assets/Scripts/PopFireMarbleEvent.cs
@@ -3,32 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using StoryGraph;
 
-public class PopMarbleEvent : MonoBehaviour
+public class PopFireMarbleEvent : PopMarbleEvent
 {
 
-    public Transform ThumbPoint;
-    public Transform IndexPoint;
-	public ParticleSystem particleSystem;
-    protected float _distance;
-	protected Vector3 _cacheLocalScale;
-	public bool Popped = false;
-	protected enum PoseState {Idle, OkHand};
-	protected PoseState poseState = PoseState.Idle;
+	public ParticleSystem sparkSystem;
+	public ParticleSystem flashSystem;
 
-
-	public StoryListener popListener;
-
-    void Start()
-    {
-		_cacheLocalScale = Vector3.one;
-        _distance = Vector3.Distance(ThumbPoint.position, IndexPoint.position);
-    }
-
-	public void ResetPop(){
-		Popped = false;
-	}
-
-	public virtual void CenterMarble(){
+	public override void CenterMarble(){
 		transform.position = Vector3.Lerp(ThumbPoint.position, IndexPoint.position, 0.5f);
         Vector3 _direction = (ThumbPoint.position - IndexPoint.position).normalized;
         transform.rotation = Quaternion.LookRotation(_direction);
@@ -36,7 +17,8 @@ public class PopMarbleEvent : MonoBehaviour
 		float distance = Vector3.Distance(ThumbPoint.position, IndexPoint.position); 
         
 		if (distance < _distance/4 && Popped == false){
-			particleSystem.Emit(20);
+			sparkSystem.Play();
+			flashSystem.Play();
 			Popped = true;
 		}
 		else if (distance < _distance &&  distance >= _distance/4)
