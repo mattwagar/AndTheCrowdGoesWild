1a905759731d74696a8a4b4d1483a60978a1c5f0
Matthew Wagar <mrw77@drexel.edu>
2019-05-10 06:06:04 -0400

    All Hands working just fine. Plus added a material to the ghost. And added some of the music logic

---
+++ UnityProject/Assets/Scripts/ChangeMaterialColorNode.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace StoryGraph
+{
+    public class ChangeMaterialColorNode : CoroutineNode
+    {
+
+        [StoryGraphField] public SkinnedMeshRenderer skinnedMeshRenderer;
+        [StoryGraphField] public Color color;
+        [StoryGraphField] public float Duration;
+        [StoryGraphField] public AnimationCurve AnimCurve;
+        
+        public override string MenuName { get { return "ChangeMaterialColorNode"; } }
+
+        public override void Execute()
+        {
+            storyGraph.StartCoroutine(FadeInTarget());
+        }
+        public IEnumerator FadeInTarget()
+        {
+
+            float journey = 0f;
+            
+            while (journey <= Duration)
+            {
+                journey = journey + Time.deltaTime;
+                float percent = Mathf.Clamp01(journey / Duration);
+
+                float curvePercent = AnimCurve.Evaluate(percent);
+                skinnedMeshRenderer.material.color = Color.Lerp(Color.white, color, curvePercent);
+
+                yield return null;
+            }
+            GoToNextNode();
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/ChangeMusicState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StoryGraph;
+
+public class ChangeMusicState : ActionNode
+{
+
+    [StoryGraphField] public MarbleMusicManager marbleMusicManager;
+    [StoryGraphField] public MusicState musicState;
+  
+    public override string MenuName {get{return "Action/ChangeMusicState";}}
+
+
+
+    public override void Execute()
+    {
+        marbleMusicManager.musicState = musicState;
+        GoToNextNode();
+    }
+
+}
