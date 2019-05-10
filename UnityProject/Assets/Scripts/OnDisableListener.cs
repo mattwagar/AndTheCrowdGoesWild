6033815d9af93db0f780d1be113f2a3ac9e85017
Matthew Wagar <mrw77@drexel.edu>
2019-05-10 04:14:58 -0400

    Got the left hand working kinda mostly

---
+++ UnityProject/Assets/Scripts/OnDisableListener.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StoryGraph;
+
+
+public class OnDisableListener : StoryListener
+{
+
+    /// <summary>
+    /// This function is called when the object becomes enabled and active.
+    /// </summary>
+    void OnDisable()
+    {
+        if (StoryListenerAction != null)
+        {
+            StoryListenerAction.Invoke();
+        }
+    }
+
+}
+
